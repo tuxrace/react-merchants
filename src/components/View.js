bfcaf3efef36583bea0c5547318894f5d3ae6663
Gerard Banasig <gerardb@istackmanila.com>
2017-12-13 10:13:07 +0800

    Fix single Load

--- src/components/View.js
+++ src/components/View.js
@@ -8,8 +8,8 @@ import { Field, reduxForm } from 'redux-form'
 import { fromJS } from 'immutable'
 
 const singleMerchantSelector = createSelector(
-  state => state.get('singleMerchant'),
-  (substate) => { return substate }
+  state => state.toJS(),
+  (substate) => { return substate.singleMerchant }
 )
 
 const mapStateToProps = state => ({
@@ -18,39 +18,61 @@ const mapStateToProps = state => ({
 
 class View extends Component {
   componentDidMount() {
-    let { dispatch } = this.props    
+    let { dispatch } = this.props
     let action = actionCreators.loadMerchantAction(this.props.routeParams.id)
     dispatch(action)
   }
   render() {
-    
-    console.log(this.props.singleMerchant)
-    return (
+    const userInfo = this.props.singleMerchant
+    return (userInfo &&
       <div>
         <NavComponent />
         <Grid>
           <Row className="show-grid">
             <Col xs={12} md={12}>
-              <Panel header={"Add a new merchant"} bsStyle="primary">
+              <Panel header={"Merchant Information"} bsStyle="primary">
                 <Form horizontal>
                   <FormGroup controlId="formHorizontalEmail">
-                    <Col sm={2}>
-                      Email
-                </Col>
-                    <Col sm={10}>                    
-                      <FormControl type="email" placeholder="Email" value={""} />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup controlId="formHorizontalPassword">
-                    <Col sm={2}>
-                      Password
-                </Col>
+                    <Col sm={2}> First Name </Col>
                     <Col sm={10}>
-                      <FormControl type="password" placeholder="Password" />
+                      <FormControl type="text" placeholder="First Name" value={userInfo.firstName} />
                     </Col>
                   </FormGroup>
-
+                  <FormGroup controlId="formHorizontalPassword">
+                    <Col sm={2}> Last Name </Col>
+                    <Col sm={10}>
+                      <FormControl type="text" placeholder="Last Name" value={userInfo.lastName} />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup controlId="formHorizontalPassword">
+                    <Col sm={2}> Avatar Url </Col>
+                    <Col sm={10}>
+                      <FormControl type="text" placeholder="Last Name" value={userInfo.avatarUrl} />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup controlId="formHorizontalPassword">
+                    <Col sm={2}> Email </Col>
+                    <Col sm={10}>
+                      <FormControl type="text" placeholder="Last Name" value={userInfo.email} />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup controlId="formHorizontalPassword">
+                    <Col sm={2}> Phone </Col>
+                    <Col sm={10}>
+                      <FormControl type="text" placeholder="Last Name" value={userInfo.phone} />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup controlId="formHorizontalPassword">
+                    <Col sm={2}> Has Premium </Col>
+                    <Col sm={10}>
+                      <FormControl type="text" placeholder="Last Name" value={userInfo.hasPremium} />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup controlId="formHorizontalPassword">                    
+                    <Col sm={12}>
+                      Bids
+                    </Col>
+                  </FormGroup>
                 </Form>
               </Panel>
             </Col>
